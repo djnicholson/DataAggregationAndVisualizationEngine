ce1a2789070831bdb85bf0dacd76a56873da0732
David Nicholson <me@djnicholson.com>
2016-03-09 17:28:11 -0800

    library test project

---
+++ src/DAaVE.Library.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="AssemblyInfo.cs" company="David Nicholson">
+//     Copyright (c) David Nicholson. All rights reserved.
+// </copyright>
+// <summary>Assembly-level attributes.</summary>
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Data Aggregation and Visualization Engine Library Unit Tests")]
+[assembly: AssemblyCompany("David Nicholson")]
+[assembly: AssemblyProduct("Data Aggregation and Visualization Engine (DAaVE)")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d85bd173-fd09-4e8a-b00a-24c4d87132a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
