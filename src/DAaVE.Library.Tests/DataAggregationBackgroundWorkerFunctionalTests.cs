5e6e933f1891ad207ba15e257ffbcfd14c9ced23
David Nicholson <me@djnicholson.com>
2016-03-13 18:29:48 -0700

    DataAggregationBackgroundWorkerFunctionalTests skeleton

---
+++ src/DAaVE.Library.Tests/DataAggregationBackgroundWorkerFunctionalTests.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="DataAggregationBackgroundWorkerFunctionalTests.cs" company="David Nicholson">
+//     Copyright (c) David Nicholson. All rights reserved.
+// </copyright>
+// <summary>See class header.</summary>
+
+namespace DAaVE.Library.Tests
+{
+    using System;
+
+    using DAaVE.Library.DataAggregation;
+    using DAaVE.Samples;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Functional tests (make and depend on assumptions about the shared thread pool) for the 
+    /// <see cref="DataAggregationBackgroundWorker{TDataPointTypeEnum}"/> class.
+    /// </summary>
+    [TestClass]
+    public class DataAggregationBackgroundWorkerFunctionalTests
+    {
+    }
+}
