e8d324cd1802f031075fb9911768c9df031a0ae7
David Nicholson <me@djnicholson.com>
2016-03-16 20:15:28 -0700

    DataAggregationBackgroundWorkerFunctionalTests.SmokeTest

--- src/DAaVE.Library.Tests/DataAggregationBackgroundWorkerFunctionalTests.cs
+++ src/DAaVE.Library.Tests/DataAggregationBackgroundWorkerFunctionalTests.cs
@@ -8,10 +8,11 @@ namespace DAaVE.Library.Tests
     using System;
 
     using DAaVE.Library.DataAggregation;
+    using DAaVE.Library.ErrorHandling.ErrorSinks;
     using DAaVE.Samples;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+    
     /// <summary>
     /// Functional tests (make and depend on assumptions about the shared thread pool) for the 
     /// <see cref="DataAggregationBackgroundWorker{TDataPointTypeEnum}"/> class.
@@ -19,5 +20,20 @@ namespace DAaVE.Library.Tests
     [TestClass]
     public class DataAggregationBackgroundWorkerFunctionalTests
     {
+        /// <summary>
+        /// Basic sanity test that an instance of <see cref="DataAggregationBackgroundWorker{TDataPointTypeEnum}"/> can successfully
+        /// be initialized and disposed (without any actual verification of what it does).
+        /// </summary>
+        [TestMethod]
+        public void SmokeTest()
+        {
+            var type = SampleDataPointType.PriceOfBitcoin;
+            var aggregator = new NoOpAggregator();
+            var errorSink = new CallbackErrorSink((message, hasException, exception) => { });
+            using (var pager = new SampleDataPointPager<SampleDataPointType>())
+            using (var target = new DataAggregationBackgroundWorker<SampleDataPointType>(type, aggregator, pager, errorSink))
+            {
+            }
+        }
     }
 }
---
+++ src/DAaVE.Library/ErrorHandling/ErrorSinks/CallbackErrorSink.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="CallbackErrorSink.cs" company="David Nicholson">
+//     Copyright (c) David Nicholson. All rights reserved.
+// </copyright>
+// <summary>See class header.</summary>
+
+namespace DAaVE.Library.ErrorHandling.ErrorSinks
+{
+    using System;
+
+    /// <summary>
+    /// Invokes a callback whenever there is an error.
+    /// </summary>
+    public sealed class CallbackErrorSink : IErrorSink
+    {
+        /// <summary>
+        /// Client-supplied code that is invoked whenever there is an error.
+        /// </summary>
+        private readonly Action<string, bool, Exception> onError;
+
+        /// <summary>
+        /// Initializes a new instance of the CallbackErrorSink class.
+        /// </summary>
+        /// <param name="onError">
+        /// Code that is invoked whenever there is an error. 
+        /// The first parameter is the error message.
+        /// The second parameter indicates whether an <see cref="Exception"/> is available.
+        /// The third parameter is the <see cref="Exception"/> thrown (if available).
+        /// </param>
+        public CallbackErrorSink(Action<string, bool, Exception> onError)
+        {
+            if (onError == null)
+            {
+                throw new ArgumentNullException("onError");
+            }
+
+            this.onError = onError;
+        }
+
+        /// <inheritdoc/>
+        public void OnError(string message)
+        {
+            this.OnError(message, exception: null);
+        }
+
+        /// <inheritdoc/>
+        public void OnError(string message, Exception exception)
+        {
+            this.onError(message, exception != null, exception);
+        }
+    }
+}
